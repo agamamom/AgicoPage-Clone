afc8e4823d5eaa0095bef1d9fcf16e8647a14485
Dat <dathoang31082001@gmail.com>
2022-03-02 11:48:00 +0700

    update code

---
+++ js/main.js
@@ -0,0 +1,37 @@
+window.onscroll = () => {
+
+
+    if (window.scrollY > 0) {
+        document.querySelector('.header').classList.add('active');
+    } else {
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+
+window.onload = () => {
+    if (window.scrollY > 0) {
+        document.querySelector('.header').classList.add('active');
+    } else {
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.add('active');
+}
+
+document.querySelector('#nav-close').onclick = () => {
+    navbar.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.add('active');
+}
+
+document.querySelector('#close-search').onclick = () => {
+    searchForm.classList.remove('active');
+}
