a98e220d6e2e852e9253e2b374d9cc2b0674a3c6
Dat <dathoang31082001@gmail.com>
2022-03-05 10:52:51 +0700

    update banner

--- js/main.js
+++ js/main.js
@@ -1,37 +1,16 @@
-window.onscroll = () => {
+// window.onscroll = () => {
 
+//     if (window.scrollY > 0) {
+//         document.querySelector('.header').classList.add('active');
+//     } else {
+//         document.querySelector('.header').classList.remove('active');
+//     }
+// };
 
-    if (window.scrollY > 0) {
-        document.querySelector('.header').classList.add('active');
-    } else {
-        document.querySelector('.header').classList.remove('active');
-    }
-};
-
-window.onload = () => {
-    if (window.scrollY > 0) {
-        document.querySelector('.header').classList.add('active');
-    } else {
-        document.querySelector('.header').classList.remove('active');
-    }
-};
-
-let navbar = document.querySelector('.header .navbar');
-
-document.querySelector('#menu-btn').onclick = () => {
-    navbar.classList.add('active');
-}
-
-document.querySelector('#nav-close').onclick = () => {
-    navbar.classList.remove('active');
-}
-
-let searchForm = document.querySelector('.search-form');
-
-document.querySelector('#search-btn').onclick = () => {
-    searchForm.classList.add('active');
-}
-
-document.querySelector('#close-search').onclick = () => {
-    searchForm.classList.remove('active');
-}
+// window.onload = () => {
+//     if (window.scrollY > 0) {
+//         document.querySelector('.header').classList.add('active');
+//     } else {
+//         document.querySelector('.header').classList.remove('active');
+//     }
+// };
