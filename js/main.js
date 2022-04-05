07e1c713081b8f4d6c5e4bb4bcdbe0b2b7eb8a44
Dat <dathoang31082001@gmail.com>
2022-04-05 15:06:17 +0700

    update header scroll down

--- js/main.js
+++ js/main.js
@@ -22,3 +22,20 @@
 // iconBtn.classList.add('fas', 'fa-long-arrow-alt-right');
 // iconBtn.ariaHidden = "false";
 // console.log(iconBtn);
+
+window.onscroll = () => {
+
+    if (window.scrollY > 0) {
+        document.querySelector('.header').classList.add('active');
+    } else {
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+
+window.onload = () => {
+    if (window.scrollY > 0) {
+        document.querySelector('.header').classList.add('active');
+    } else {
+        document.querySelector('.header').classList.remove('active');
+    }
+};
